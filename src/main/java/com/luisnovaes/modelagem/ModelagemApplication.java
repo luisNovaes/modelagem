fe0b612991f9deafc0d44604cc1080d8cc3acddc
Luis Magno Novaes <luismagnovaes@gmail.com>
2018-07-15 22:35:07 -0300

    Projeto criado

---
+++ src/main/java/com/luisnovaes/modelagem/ModelagemApplication.java
@@ -0,0 +1,12 @@
+package com.luisnovaes.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemApplication.class, args);
+	}
+}
