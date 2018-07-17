85e0e7267e674d59373bc1b1287cfb779efae8f9
Luis Magno Novaes <luismagnovaes@gmail.com>
2018-07-17 19:53:36 -0300

    Criando operação de instanciaçao

--- src/main/java/com/luisnovaes/modelagem/ModelagemApplication.java
+++ src/main/java/com/luisnovaes/modelagem/ModelagemApplication.java
@@ -1,12 +1,33 @@
 package com.luisnovaes.modelagem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luisnovaes.modelagem.domain.Categoria;
+import com.luisnovaes.modelagem.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemApplication {
+public class ModelagemApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
 	}
 }
